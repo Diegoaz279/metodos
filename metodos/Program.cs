c1aa2a3b1021a3694641be58d12aecce2376411c
Diego Castillo <Diegoaz279@gmail.com>
2024-06-07 16:57:46 -0400

    Agregar archivos de proyecto.

---
+++ metodos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Solicitar_Apellido_Nombre
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            static string pedirnombre()
+            {
+                Console.WriteLine("Digite su nombre: \n");
+                return Console.ReadLine();
+            }
+
+            static string pedirapellido()
+            {
+                Console.WriteLine("Digite su apellido: \n");
+                return Console.ReadLine();
+            }
+
+            string nombre   = pedirnombre();
+            string apellido = pedirapellido();
+
+            Console.WriteLine("\n Hola " + nombre + " " + apellido+ " ");
+            Console.ReadLine(); 
+        }
+    }
+}
